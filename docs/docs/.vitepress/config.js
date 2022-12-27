66a164982d61f3b981488e583f5c9128003c1f48
黄嘉铭 <347975891@qq.com>
2022-12-27 10:23:39 +0800

    test

--- docs/docs/.vitepress/config.js
+++ docs/docs/.vitepress/config.js
@@ -1,5 +1,50 @@
 export default {
   title: 'VitePress',
   description: 'Just playing around.',
-  base: '/h5-component/docs/'
+  base: '/h5-component/',
+  themeConfig: {
+    siteTitle: false,
+    logo: '/logo.svg',
+    nav: [
+      { text: 'Guide', link: '/guide' },
+      { text: 'Configs', link: '/configs' },
+      { text: 'Changelog', link: 'https://github.com/...' },
+      {
+        text: 'Dropdown Menu',
+        items: [
+          { text: 'Item A', link: '/item-1' },
+          { text: 'Item B', link: '/item-2' },
+          { text: 'Item C', link: '/item-3' }
+        ]
+      }
+    ],
+    sidebar: [
+      {
+        text: 'Section Title A',
+        items: [
+          { text: 'Item A', link: '/item-a' },
+          { text: 'Item B', link: '/item-b' },
+        ]
+      },
+      {
+        text: 'Section Title B',
+        items: [
+          { text: 'Item C', link: '/item-c' },
+          { text: 'Item D', link: '/item-d' },
+        ]
+      }
+    ],
+    editLink: {
+      pattern: 'https://github.com/vuejs/vitepress/edit/main/docs/:path',
+      text: 'Edit this page on GitHub'
+    },
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2019-present Evan You'
+    },
+    carbonAds: {
+      code: 'your-carbon-code',
+      placement: 'your-carbon-placement'
+    }
+  }
 }
--- docs/docs/index.md
+++ docs/docs/index.md
@@ -1,5 +1,15 @@
-# Hello VitePress
+---
+title: Docs with VitePress
+editLink: true
+---
+
+# Hello {{ $frontmatter.title }}
 
 ## hhhhhh
 
 ### test
+
+### Title <Badge type="info" text="default" />
+### Title <Badge type="tip" text="^1.9.0" />
+### Title <Badge type="warning" text="beta" />
+### Title <Badge type="danger" text="caution" />
