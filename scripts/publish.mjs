35e777d2441f8c2a6695adbe3a76e93dd30c2dc9
黄嘉铭 <347975891@qq.com>
2022-12-29 09:55:27 +0800

    chore(release): 0.0.10

--- scripts/publish.mjs
+++ scripts/publish.mjs
@@ -5,13 +5,15 @@ import path from 'path'
 
 const pkgPath = path.join(process.cwd(), 'package.json')
 
-const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf8'))
+let pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf8'))
 
 pkg.name = '@gurming/h5-component'
 
 fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2))
 
 await $`version-publish`
+
+pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf8'))
 
 delete pkg.name
 
